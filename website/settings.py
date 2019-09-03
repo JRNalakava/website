a82fb5558ceb2b0bedb91b8c916852d3f7f23de6
Joao <joao.rangelgs@gmail.com>
2019-09-03 17:38:50 -0500

    removed submit from rushee application. added password reset default page TODO: Change password reset template

--- website/settings.py
+++ website/settings.py
@@ -29,10 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kn)$pdxsbx_tcrw3@qq4tv71ho0tu$v1svtzmrjnr_!5h^d^##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = ['iota.herokuapp.com']
-
 # Application definition
 
 INSTALLED_APPS = [
