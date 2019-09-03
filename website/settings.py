da1d16f7c855e8044062b2c827080dfeb45a78bf
Joao <joao.rangelgs@gmail.com>
2019-09-03 17:22:25 -0500

    removed submit from rushee application. added password reset default page TODO: Change password reset template

--- website/settings.py
+++ website/settings.py
@@ -126,12 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'utakpsi@gmail.com'
-EMAIL_HOST_PASSWORD = 'texasakpsi15'
+EMAIL_USE_TLS = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
